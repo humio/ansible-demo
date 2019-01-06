478481acb0e08ce03112645f6aba1f0c5c62ea09
Greg Burd <gburd@humio.com>
2019-01-05 21:27:34 -0500

    WIP

--- aws_ec2/zfs-instance.sh
+++ aws_ec2/zfs-instance.sh
@@ -57,6 +57,8 @@ zpool create ${POOL} raidz2 -f \
     -O atime=off \
     -O compression=off \
     -O xattr=off \
+    -O checksum=sha256 \
+    -O recordsize=1M \
     ${EBS_DEVS} \
     cache ${EPH_DEVS}
 
@@ -120,10 +122,110 @@ options zfs zfs_vdev_sync_write_max_active=100 #was 10
 options zfs zfs_vdev_max_active=4000 #was 1000
 EOF
 
+cat > /etc/sysctl.d/10-perf.conf <<EOF
+### KERNEL TUNING ###
+
+# Increase size of file handles and inode cache
+fs.file-max = 2097152
+
+# Do less swapping
+vm.swappiness = 10
+vm.dirty_ratio = 60
+vm.dirty_background_ratio = 2
+
+# Sets the time before the kernel considers migrating a proccess to another core
+kernel.sched_migration_cost_ns = 5000000
+
+# Group tasks by TTY
+#kernel.sched_autogroup_enabled = 0
+
+### GENERAL NETWORK SECURITY OPTIONS ###
+
+# Number of times SYNACKs for passive TCP connection.
+net.ipv4.tcp_synack_retries = 2
+
+# Allowed local port range
+net.ipv4.ip_local_port_range = 2000 65535
+
+# Protect Against TCP Time-Wait
+net.ipv4.tcp_rfc1337 = 1
+
+# Control Syncookies
+net.ipv4.tcp_syncookies = 1
+
+# Decrease the time default value for tcp_fin_timeout connection
+net.ipv4.tcp_fin_timeout = 15
+
+# Decrease the time default value for connections to keep alive
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_intvl = 15
+
+### TUNING NETWORK PERFORMANCE ###
+
+# Default Socket Receive Buffer
+net.core.rmem_default = 31457280
+
+# Maximum Socket Receive Buffer
+net.core.rmem_max = 33554432
+
+# Default Socket Send Buffer
+net.core.wmem_default = 31457280
+
+# Maximum Socket Send Buffer
+net.core.wmem_max = 33554432
+
+# Increase number of incoming connections
+net.core.somaxconn = 65535
+
+# Increase number of incoming connections backlog
+net.core.netdev_max_backlog = 65536
+
+# Increase the maximum amount of option memory buffers
+net.core.optmem_max = 25165824
+
+# Increase the maximum total buffer-space allocatable
+# This is measured in units of pages (4096 bytes)
+net.ipv4.tcp_mem = 786432 1048576 26777216
+net.ipv4.udp_mem = 65536 131072 262144
+
+# Increase the read-buffer space allocatable
+net.ipv4.tcp_rmem = 8192 87380 33554432
+net.ipv4.udp_rmem_min = 16384
+
+# Increase the write-buffer-space allocatable
+net.ipv4.tcp_wmem = 8192 65536 33554432
+net.ipv4.udp_wmem_min = 16384
+
+# Increase the tcp-time-wait buckets pool size to prevent simple DOS attacks
+net.ipv4.tcp_max_tw_buckets = 1440000
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+
+# Use TCP BBR Congestion Control
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+cat > /etc/sysctl.d/10-humio.conf <<EOF
+net.core.somaxconn = 1024
+net.core.netdev_max_backlog = 5000
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_wmem = 4096 12582912 16777216
+net.ipv4.tcp_rmem = 4096 12582912 16777216
+net.ipv4.tcp_max_syn_backlog = 8096
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 10240 65535
+EOF
+
+sysctl --system
+
 sudo zfs create -o canmount=off -o setuid=off -o exec=off tank/var
-sudo zfs create -o com.sun:auto-snapshot=false -o setuid=off -o mountpoint=/var/humio tank/var/humio
-sudo zfs create -o com.sun:auto-snapshot=false -o setuid=off -o mountpoint=/var/kafka tank/var/kafka
-sudo zfs create -o com.sun:auto-snapshot=false -o setuid=off -o mountpoint=/var/lib/zookeeper tank/var/zookeeper
+sudo zfs create -o com.sun:auto-snapshot=false -o mountpoint=/var/humio tank/var/humio
+sudo zfs create -o com.sun:auto-snapshot=false -o mountpoint=/var/kafka tank/var/kafka
+sudo zfs create -o com.sun:auto-snapshot=false -o mountpoint=/var/lib/zookeeper tank/var/zookeeper
 
 # resource "aws_instance" "zk-kafka-humios" {
 #  provisioner "local-exec" {
