3d85d1176f619940d2b16adf04962d67f9f9306d
Jestin Woods <jestin@humio.com>
2019-08-02 10:54:32 -0700

    Move kms key to bootstrap

--- gcp-bootstrap/main.tf
+++ gcp-bootstrap/main.tf
@@ -11,6 +11,21 @@ resource "google_storage_bucket" "bucket" {
   }
 }
 
+# During initial provision, the kms key must be a resource. After that it is a datasource. Unfortunately,
+# google doesn't allow the deletion of kms keys or key-rings, so we must do this (or delete and recreate the project
+# completely).
+
+resource "google_kms_key_ring" "humio" {
+  name     = "humio-key-ring"
+  location = "${var.region}"
+}
+
+resource "google_kms_crypto_key" "humio" {
+  name            = "humio-key"
+  key_ring        = "${data.google_kms_key_ring.humio.self_link}"
+  rotation_period = "100000s"
+}
+
 output "bucket_name" {
   value = "${google_storage_bucket.bucket.name}"
 }
--- gcp/kms.tf
+++ gcp/kms.tf
@@ -6,26 +6,9 @@ data "google_kms_key_ring" "humio" {
 }
 
 data "google_kms_crypto_key" "humio" {
-  name            = "humio-key"
-  key_ring        = "${data.google_kms_key_ring.humio.self_link}"
+  name     = "humio-key"
+  key_ring = "${data.google_kms_key_ring.humio.self_link}"
 }
-
-# During initial provision, the kms key must be a resource. After that it is a datasource. Unfortunately,
-# google doesn't allow the deletion of kms keys or key-rings, so we must do this (or delete and recreate the project
-# completely).
-#resource "google_kms_key_ring" "humio" {
-#  name     = "humio-key-ring"
-#  location = "${var.region}"
-#}
-#resource "google_kms_crypto_key" "humio" {
-#  name            = "humio-key"
-#  key_ring        = "${data.google_kms_key_ring.humio.self_link}"
-#  rotation_period = "100000s"
-#
-#  lifecycle {
-#    prevent_destroy = false # Change to true to prevent accidental destruction of KMS key
-#  }
-#}
 
 resource "google_kms_crypto_key_iam_binding" "encrypter_decrypter" {
   crypto_key_id = "${data.google_kms_crypto_key.humio.self_link}"
