f835f83424a9cd85327d6c2400e1adc788799327
Grant Schofield <grant@surlysoft.com>
2019-08-21 17:45:43 -0700

    revert bucket polocy

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,5 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
+ version = "~> 2.11"
  credentials = "${file(var.gcp_credentials)}"
  project     = "${var.gcp_project_id}"
  region      = "${var.region}"
