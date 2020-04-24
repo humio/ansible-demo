6d5b0af906e7f76c7ad1c122548629a7aa4df985
imorti <imorti@gmail.com>
2020-04-24 16:10:52 -0700

    updated to terraform 0.12, added some ps tagging and testing full install works.

--- aws_ec2/main.tf
+++ aws_ec2/main.tf
@@ -1,32 +1,34 @@
-provider "aws" {}
+provider "aws" {
+}
 
 data "aws_ami" "ami" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["${var.aws_ami_filter}"]
+    values = [var.aws_ami_filter]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-//  owners = ["099720109477"]
+  
+  owners = ["099720109477"]
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+}
 
 resource "aws_vpc" "humio" {
-  cidr_block = "${var.aws_vpc_cidr_block}"
+  cidr_block           = var.aws_vpc_cidr_block
   enable_dns_hostnames = true
 }
 
 resource "aws_security_group" "main" {
   name        = "${var.aws_name_prefix}allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 22
@@ -36,57 +38,57 @@ resource "aws_security_group" "main" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "zookeepers" {
   name        = "zookeepers"
   description = "Allow Zookeeper inbound traffic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 2181
     to_port     = 2181
     protocol    = "TCP"
-    cidr_blocks = ["${aws_vpc.humio.cidr_block}"]
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 
   ingress {
     from_port   = 2888
     to_port     = 2888
     protocol    = "TCP"
-    cidr_blocks = ["${aws_vpc.humio.cidr_block}"]
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 
   ingress {
     from_port   = 3888
     to_port     = 3888
     protocol    = "TCP"
-    cidr_blocks = ["${aws_vpc.humio.cidr_block}"]
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 }
 
 resource "aws_security_group" "kafkas" {
   name        = "kafkas"
   description = "Allow Kafka inbound traffic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 9092
     to_port     = 9092
     protocol    = "TCP"
-    cidr_blocks = ["${aws_vpc.humio.cidr_block}"]
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 }
 
 resource "aws_security_group" "humios" {
   name        = "humios"
   description = "Allow Humio inbound traffic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 8080
@@ -106,7 +108,7 @@ resource "aws_security_group" "humios" {
 resource "aws_security_group" "ui-public" {
   name        = "humio-ui-public"
   description = "Allow humio public trafic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 80
@@ -116,17 +118,17 @@ resource "aws_security_group" "ui-public" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["${aws_vpc.humio.cidr_block}"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 }
 
 resource "aws_security_group" "ingest-public" {
   name        = "humio-ingest-public"
   description = "Allow humio public trafic"
-  vpc_id      = "${aws_vpc.humio.id}"
+  vpc_id      = aws_vpc.humio.id
 
   ingress {
     from_port   = 80
@@ -143,61 +145,62 @@ resource "aws_security_group" "ingest-public" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["${aws_vpc.humio.cidr_block}"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.humio.cidr_block]
   }
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = "${aws_vpc.humio.id}"
+  vpc_id = aws_vpc.humio.id
 }
 
 resource "aws_route_table" "route_table" {
-  vpc_id = "${aws_vpc.humio.id}"
+  vpc_id = aws_vpc.humio.id
 }
 
 resource "aws_route" "internet_route" {
-  route_table_id = "${aws_route_table.route_table.id}"
-  gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+  route_table_id         = aws_route_table.route_table.id
+  gateway_id             = aws_internet_gateway.internet_gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
 resource "aws_route_table_association" "primary" {
-  count = 2
-  route_table_id = "${aws_route_table.route_table.id}"
-  subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
+  count          = 2
+  route_table_id = aws_route_table.route_table.id
+  subnet_id      = element(aws_subnet.subnets.*.id, count.index)
 }
 
 resource "aws_subnet" "subnets" {
-  count = "${var.zones}"
-  vpc_id = "${aws_vpc.humio.id}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  cidr_block = "${cidrsubnet(aws_vpc.humio.cidr_block, 8, count.index+1)}"
+  count                   = var.zones
+  vpc_id                  = aws_vpc.humio.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.humio.cidr_block, 8, count.index + 1)
   map_public_ip_on_launch = true
 }
 
 resource "aws_lb" "ui" {
-  name = "humio-ui"
-  internal = false
+  name               = "humio-ui"
+  internal           = false
   load_balancer_type = "application"
-  subnets = ["${aws_subnet.subnets.*.id}"]
-  security_groups = ["${aws_security_group.ui-public.id}"]
+  subnets            = aws_subnet.subnets.*.id
+  security_groups    = [aws_security_group.ui-public.id]
 }
 
 resource "aws_lb" "ingest" {
-  name = "humio-ingest"
-  internal = false
+  name               = "humio-ingest"
+  internal           = false
   load_balancer_type = "application"
-  subnets = ["${aws_subnet.subnets.*.id}"]
-  security_groups = ["${aws_security_group.ingest-public.id}"]
+  subnets            = aws_subnet.subnets.*.id
+  security_groups    = [aws_security_group.ingest-public.id]
 }
 
 resource "aws_lb_target_group" "ui-http" {
-  name = "humio-ui"
-  port = 8080
+  name     = "humio-ui"
+  port     = 8080
   protocol = "HTTP"
-  vpc_id = "${aws_vpc.humio.id}"
+  vpc_id   = aws_vpc.humio.id
   health_check {
     path = "/api/v1/status"
   }
@@ -207,106 +210,114 @@ resource "aws_lb_target_group" "ui-http" {
 }
 
 resource "aws_lb_target_group" "ingest-api" {
-  name = "humio-ingest-api"
-  port = 8080
+  name     = "humio-ingest-api"
+  port     = 8080
   protocol = "HTTP"
-  vpc_id = "${aws_vpc.humio.id}"
+  vpc_id   = aws_vpc.humio.id
   health_check {
     path = "/api/v1/status"
   }
 }
 
 resource "aws_lb_target_group" "ingest-es" {
-  name = "humio-ingest-es"
-  port = 9200
+  name     = "humio-ingest-es"
+  port     = 9200
   protocol = "HTTP"
-  vpc_id = "${aws_vpc.humio.id}"
+  vpc_id   = aws_vpc.humio.id
   health_check {
-    path = "/_bulk"
+    path    = "/_bulk"
     matcher = "200-499"
   }
 }
 
 resource "aws_lb_listener" "humio-ui-http" {
-  load_balancer_arn = "${aws_lb.ui.arn}"
-  port = 80
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.ui.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  "default_action" {
-    type = "forward"
-    target_group_arn = "${aws_lb_target_group.ui-http.arn}"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ui-http.arn
   }
 }
 
 resource "aws_lb_listener" "humio-ingest-http" {
-  load_balancer_arn = "${aws_lb.ingest.arn}"
-  port = 80
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.ingest.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  "default_action" {
-    type = "forward"
-    target_group_arn = "${aws_lb_target_group.ingest-api.arn}"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingest-api.arn
   }
 }
-resource "aws_lb_listener" "humio-ingest-es" {
-  load_balancer_arn = "${aws_lb.ingest.arn}"
-  port = 9200
-  protocol = "HTTP"
 
-  "default_action" {
-    type = "forward"
-    target_group_arn = "${aws_lb_target_group.ingest-es.arn}"
+resource "aws_lb_listener" "humio-ingest-es" {
+  load_balancer_arn = aws_lb.ingest.arn
+  port              = 9200
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingest-es.arn
   }
 }
 
 resource "aws_network_interface" "nic" {
-  count = "${var.instances}"
-  subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
-  private_ips = ["${cidrhost(element(aws_subnet.subnets.*.cidr_block, count.index), count.index+10)}"]
+  count     = var.instances
+  subnet_id = element(aws_subnet.subnets.*.id, count.index)
+  private_ips = [cidrhost(
+    element(aws_subnet.subnets.*.cidr_block, count.index),
+    count.index + 10,
+  )]
   security_groups = [
-    "${aws_security_group.main.id}",
-    "${ count.index < var.zookeepers ? aws_security_group.zookeepers.id : "" }",
-    "${aws_security_group.kafkas.id}",
-    "${aws_security_group.humios.id}"
+    aws_security_group.main.id,
+    count.index < var.zookeepers ? aws_security_group.zookeepers.id : "",
+    aws_security_group.kafkas.id,
+    aws_security_group.humios.id,
   ] //TODO: Replace humio_security_group with ELBs
 }
 
 resource "aws_instance" "humios" {
-  count = "${var.instances}"
-  ami = "${data.aws_ami.ami.image_id}"
-  instance_type = "${var.humio_plan}"
-  key_name = "${var.aws_key_name}"
+  count         = var.instances
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.humio_plan
+  key_name      = var.aws_key_name
 
   user_data = <<USERDATA
 #!/bin/bash
 test -e /usr/bin/python || (apt -y update && apt install -y python-minimal python-apt)
 USERDATA
 
+
   network_interface {
-    device_index = 0
-    network_interface_id = "${element(aws_network_interface.nic.*.id, count.index)}"
+    device_index         = 0
+    network_interface_id = element(aws_network_interface.nic.*.id, count.index)
   }
 
-  tags {
-    Name = "${format("humio%02d", count.index + 1)}"
-    cluster_index = "${count.index + 1}"
+  tags = {
+    Name          = format("humio%02d", count.index + 1)
+    cluster_index = count.index + 1
+    Group = "professional services"
   }
 }
 
 resource "aws_lb_target_group_attachment" "zkh-ui" {
-  target_group_arn = "${aws_lb_target_group.ui-http.arn}"
-  target_id = "${element(aws_instance.humios.*.id, count.index)}"
-  count = "${var.instances}"
+  target_group_arn = aws_lb_target_group.ui-http.arn
+  target_id        = element(aws_instance.humios.*.id, count.index)
+  count            = var.instances
 }
+
 resource "aws_lb_target_group_attachment" "zkh-ingest-http" {
-  target_group_arn = "${aws_lb_target_group.ingest-api.arn}"
-  target_id = "${element(aws_instance.humios.*.id, count.index)}"
-  count = "${var.instances}"
+  target_group_arn = aws_lb_target_group.ingest-api.arn
+  target_id        = element(aws_instance.humios.*.id, count.index)
+  count            = var.instances
 }
+
 resource "aws_lb_target_group_attachment" "zkh-ingest-es" {
-  target_group_arn = "${aws_lb_target_group.ingest-es.arn}"
-  target_id = "${element(aws_instance.humios.*.id, count.index)}"
-  count = "${var.instances}"
+  target_group_arn = aws_lb_target_group.ingest-es.arn
+  target_id        = element(aws_instance.humios.*.id, count.index)
+  count            = var.instances
 }
 
 output "humio_ui" {
@@ -320,3 +331,4 @@ output "humio_ingest_api" {
 output "humio_ingest_elastic" {
   value = "http://${aws_lb.ingest.dns_name}:9200"
 }
+
--- aws_ec2/variables.tf
+++ aws_ec2/variables.tf
@@ -1,45 +1,49 @@
 variable "humio_plan" {
-  type = "string"
-  default = "m4.xlarge"
+  type        = string
+  default     = "m4.xlarge"
   description = <<EOS
 The list of Packet machines. Check curl -s -H "Accept: application/json" -H "X-Auth-Token: $\{TF_VAR_packet_auth_token\}" "https://api.packet.net/plans" | jq '.plans[] | [.name, .slug, .description, .pricing.hour]' to see a list of machines
 EOS
+
 }
 
 variable "aws_key_name" {
-  type = "string"
+  type = string
 }
 
 variable "aws_vpc_cidr_block" {
-  type = "string"
+  type    = string
   default = "172.16.0.0/16"
 }
 
 variable "aws_name_prefix" {
-  type = "string"
+  type    = string
   default = ""
 }
 
 variable "aws_ami_filter" {
-  type = "string"
-  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"
+  type        = string
+  default     = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"
   description = <<DESCRIPTION
 Options:
 * ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*
 * RHEL-7.5_HVM_GA-*-x86_64-1-Hourly2-GP2
 DESCRIPTION
+
 }
 
 variable "zookeepers" {
-  type = "string"
+  type    = string
   default = "3"
 }
+
 variable "instances" {
-  type = "string"
+  type    = string
   default = "8"
 }
 
 variable "zones" {
-  type = "string"
+  type    = string
   default = "2"
 }
+
